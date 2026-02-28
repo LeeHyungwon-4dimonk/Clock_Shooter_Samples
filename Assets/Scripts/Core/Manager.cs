22153e3d71274cef8a4961ab57558cfc068aa33c
LeeHyungwon-4dimonk <dlguddnjs1998@gmail.com>
2026-02-28 22:27:00 +0900

    [Docs] File Arrange

--- Assets/Scripts/Core/Manager.cs
+++ Assets/Scripts/Core/Manager.cs
@@ -2,6 +2,5 @@ public static class Manager
 {
     public static GameManager Game => GameManager.Instance;
     public static PoolManager Pool => PoolManager.Instance;
-
     public static StatusManager Status => StatusManager.Instance;
 }
